533f3506b96d357262e775a687a32dbdef7d1e9c
Jordan Fernandes <jfernan6@stevens.edu>
2022-04-04 14:07:11 -0400

    Added mern backend per tutorial.

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,11 @@
 import { router as posts } from "./posts";
 import { router as meals } from "./meals";
+import { router as users } from "./users";
 
 export const constructorMethod = (app) => {
     app.use("/api/posts", posts);
     app.use("/api/meals", meals);
+    app.use("/api/users", users);
     app.use("*", (req, res) => {
         res.status(404).json({ error: "Not found" });
     });
---
+++ services/users.ts
@@ -0,0 +1,33 @@
+import { users } from "../config/mongoCollections";
+const bcrypt = require("bcrypt");
+const { ObjectId } = require("mongodb");
+
+export const createUser = async (
+    username: string,
+    email: string,
+    password: string,
+) => {
+    const userCollection = await users();
+    const takenUsername = await userCollection.findOne({
+        "username": username,
+    });
+    const takenEmail = await userCollection.findOne({
+        "email": email,
+    });
+    if (takenUsername || takenEmail) {
+        throw "Username or email already taken";
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = {
+        _id: ObjectId(),
+        username: username,
+        email: email,
+        password: hashedPassword
+    };
+    const insertInfo = await userCollection.insertOne(newUser);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+        throw "Could not create user";
+    }
+    const IDAsString = insertInfo.insertedId.toString();
+    return IDAsString;
+};
