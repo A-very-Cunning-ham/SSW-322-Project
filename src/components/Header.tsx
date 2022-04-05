748ae4f4fc4ca15bd84d7b301d8850134ddbb1a5
A-very-Cunning-ham <ave210knife@gmail.com>
2022-04-05 00:32:18 -0400

    Add MUI icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-
 import React, { ReactElement, FC } from "react";
+import {AppBar, Box, Toolbar, IconButton, Typography, Button} from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu';
 
 // define interface to represent component props
 interface Props {
@@ -8,9 +9,25 @@ interface Props {
 
 const Header: FC<Props> = ({ title }): ReactElement => {
     return (
-        <div>
-            {title}
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {title}
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
     );
 };
 
