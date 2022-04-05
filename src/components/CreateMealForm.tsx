2466969045c67883d549878f5505f08fa1e158b5
Adrian <adgarcia1119@gmail.com>
2022-04-05 12:12:24 -0400

    added image upload button and checkbox forms

--- src/components/CreateMealForm.tsx
+++ src/components/CreateMealForm.tsx
@@ -1,15 +1,30 @@
 import { Button, TextField } from '@mui/material/';
 import * as React from 'react';
 import {Formik, Form} from 'formik';
-import { json } from 'stream/consumers';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
 interface Values {
     mealName: string
     description: string
-    price: any
-    dietaryRestrictions: string
-    startTime: any
-    endTime: any
+    price: string
+    startTime: string
+    endTime: string
     comments: string
 }
 
@@ -17,10 +32,23 @@ interface Props {
     onSubmit: (values: Values) => void;
 }
 
+const dietaryRestrictionTags = [
+    'Vegan',
+    'Vegetarian',
+    'Kosher',
+    'Low Carb',
+    'Keto',
+    'Dairy-Free',
+    'Gluten-Free',
+    'Allergy-Free'
+  ];
+
 export const CreateMealForm: React.FC<Props> = ({onSubmit}) => {
+    const [tagName, setTagName] = React.useState<string[]>([]); //for dietary restriction tags
+
     return (
         <Formik 
-            initialValues={{ mealName:'', description:'',price:'',dietaryRestrictions:'',startTime:'',endTime:'',comments:''}} 
+            initialValues={{ mealName:'', description:'',price:'',startTime:'',endTime:'',comments:''}} 
             onSubmit={values => {
                 onSubmit(values);
             }}
@@ -52,13 +80,25 @@ export const CreateMealForm: React.FC<Props> = ({onSubmit}) => {
                     onBlur={handleBlur} 
                 /></div>
                 <div>
-                <TextField 
-                    placeholder='Dietary Restrictions'
-                    name="dietaryRestrictions"
-                    value={values.dietaryRestrictions}
+                <FormControl sx={{ m: 1, width: 300 }}>
+                <InputLabel>Dietary Restrictions</InputLabel>
+                <Select
+                    multiple
+                    value={tagName}
                     onChange={handleChange}
-                    onBlur={handleBlur} 
-                /></div>
+                    input={<OutlinedInput label="Tag" />}
+                    renderValue={(selected) => selected.join(', ')}
+                    MenuProps={MenuProps}
+                >
+                    {dietaryRestrictionTags.map((tag) => (
+                    <MenuItem key={tag} value={tag}>
+                        <Checkbox checked={tagName.indexOf(tag) > -1} />
+                        <ListItemText primary={tag} />
+                    </MenuItem>
+                    ))}
+                </Select>
+                </FormControl>
+                </div>
                 <div>
                 <TextField 
                     placeholder='Start Time'
@@ -67,14 +107,40 @@ export const CreateMealForm: React.FC<Props> = ({onSubmit}) => {
                     onChange={handleChange}
                     onBlur={handleBlur} 
                 />
+                <Select
+                    onChange={handleChange}
+                    placeholder='AM'
+                >
+                    <MenuItem value={'AM'}>AM</MenuItem>
+                    <MenuItem value={'PM'}>PM</MenuItem>
+                </Select>
                 <TextField 
                     placeholder='End Time'
                     name="endTime"
                     value={values.endTime}
                     onChange={handleChange}
                     onBlur={handleBlur} 
-                /></div>
-                <Button type="submit">submit</Button>
+                />
+                <Select
+                   placeholder='AM'
+                   onChange={handleChange}
+                >
+                    <MenuItem value={'AM'}>AM</MenuItem>
+                    <MenuItem value={'PM'}>PM</MenuItem>
+                </Select></div>
+                <div>
+                <Button
+                    variant="contained"
+                    component="label"
+                    >
+                    Upload Image
+                    <input
+                        type="file"
+                        hidden
+                    />
+                </Button>
+                </div>
+                <Button type="submit">Save Changes</Button>
             </Form>
             )}
         </Formik>
--- src/pages/CreateMeal.tsx
+++ src/pages/CreateMeal.tsx
@@ -5,8 +5,8 @@ import { CreateMealForm } from '../components/CreateMealForm';
 const CreateMeal: FC<any> = (): ReactElement => {
     return (
         <div style={{textAlign: "center" }}>
-        <CreateMealForm onSubmit={( {mealName, description,price,dietaryRestrictions,startTime,endTime,comments}) => {
-            console.log(mealName, description,price,dietaryRestrictions,startTime,endTime,comments);
+        <CreateMealForm onSubmit={( {mealName, description,price,startTime,endTime,comments}) => {
+            console.log(mealName, description,price,startTime,endTime,comments);
           }}
           />
       </div>
