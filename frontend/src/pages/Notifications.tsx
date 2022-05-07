439b53c787bc0fc64e5ffcd9bae9d8ec8ab6cfb9
Jordan Fernandes <jfernan6@stevens.edu>
2022-05-06 21:58:17 -0400

    added initial work for route protection. its kinda bad, but wip

--- frontend/src/pages/Notifications.tsx
+++ frontend/src/pages/Notifications.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement, FC, useState, useEffect } from "react";
+import {useNavigate} from 'react-router-dom';
 import { Box, Button, Typography, Grid, Card, CardContent, Chip, StepIconClassKey } from "@mui/material";
 import Stack from '@mui/material/Stack';
 import Tabs from '@mui/material/Tabs';
@@ -54,7 +55,8 @@ interface guestNotifs {
     allGuestData: resData[]
 }
 
-const Notifications: FC<any> = (): ReactElement => {
+const Notifications: FC<any> = (): ReactElement | null => {
+    const navigate = useNavigate();
     const [value, setValue] = React.useState(0);
     const [hostData, setHostData] = React.useState<hostNotifs>({allHostData:[]});
     const [guestData, setGuestData] = React.useState<guestNotifs>({allGuestData:[]});
@@ -78,6 +80,19 @@ const Notifications: FC<any> = (): ReactElement => {
         }
         fetchData();
     }, [])
+
+    if (!axios.get(`api/users/loggedin`)){
+        console.log("dolphin")
+    }
+
+    // axios.get(`api/users/loggedin`)
+    //     .then((response) => {
+    //         console.log(response.data);
+    //         if (response.data){
+    //             navigate("/login");
+    //             return null;
+    //         }
+    //     })
 
     return(
         <Box sx={{ m:3 }}>
